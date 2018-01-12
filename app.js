eb90400b017ec7d814acfdaafae96dc2a0127868
Flavian Jacquot <flavian.jacquot@gmail.com>
2018-01-12 11:09:14 +0100

    adds question and reponse web socket

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ var io = require('socket.io').listen(server);
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
+	socket.emit('message', 'Vous êtes bien connecté !');
+	
+	socket.on('message', function (message) {
+        console.log('Un client me parle ! Il me dit : ' + message);
+    });	
+
 });
-
-
+    
 server.listen(8080);
